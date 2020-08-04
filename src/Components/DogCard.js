b6fd4d9f8f4134a8153a3b042ddcd5c8bed87a17
github username <github email address>
2020-08-04 06:12:55 -0700

    initial commit

---
+++ src/Components/DogCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DogCard() {
+  return (
+    <div>
+      <h2 >{/*Dog name goes here*/}</h2>
+      <img alt="" src={""} />
+      <button className="bark">Bark</button>
+    </div>
+  );
+}
+
+export default DogCard;
---
+++ src/Containers/DogsList.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class DogList extends Component {
+  render() {
+    return <div className="dogContainer">{/*list of dogs go here*/}</div>;
+  }
+}
+
+export default DogList;
